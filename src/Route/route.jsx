37822f8f6505abd37f6e5f5c31631dfcde64f776
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-02-19 11:36:04 +0600

    first commit

---
+++ src/Route/route.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import ContactUs from "../pages/ContactUs/ContactUs";
+import About from "../pages/About/About";
+import Blog from "../pages/Blog/Blog";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/contact",
+        element: <ContactUs />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "blog",
+        element: <Blog />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-red-400">home</div>;
+};
+
+export default Home;
